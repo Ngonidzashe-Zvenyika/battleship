e371fb19e10ac7b6a5709883300cf872e652f92a
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-11-05 13:24:34 +0200

    write test for ai to play after human and refactor previous tests

--- tests/players.test.js
+++ tests/players.test.js
@@ -2,35 +2,49 @@ import Gameboard from '../src/gameboard';
 import { HumanPlayer, AiPlayer } from '../src/player';
 
 test('Human player attacks given square on enemy board', () => {
-  const opponentBoard = new Gameboard();
-  const player = new HumanPlayer(opponentBoard);
-  player.attack([0, 0]);
-  const attackLocation = opponentBoard.missedAttacks.find((attack) => {
+  const aiBoard = new Gameboard();
+  const humanBoard = new Gameboard();
+  const ai = new AiPlayer(humanBoard);
+  const human = new HumanPlayer(ai, aiBoard);
+  human.attack([0, 0]);
+  const attackLocation = aiBoard.missedAttacks.find((attack) => {
     const [x, y] = attack;
     return x === 0 && y === 0;
   });
   expect(attackLocation).toEqual([0, 0]);
 });
 
-test("Player can't attack same square twice", () => {
-  const opponentBoard = new Gameboard();
-  const player = new HumanPlayer(opponentBoard);
-  player.attack([0, 0]);
-  expect(player.attack([0, 0])).toBe(false);
+test("Human player can't attack same square twice", () => {
+  const aiBoard = new Gameboard();
+  const humanBoard = new Gameboard();
+  const ai = new AiPlayer(humanBoard);
+  const human = new HumanPlayer(ai, aiBoard);
+  human.attack([0, 0]);
+  expect(human.attack([0, 0])).toBe(false);
 });
 
 test('Ai player attacks random square on enemy board', () => {
-  const opponentBoard = new Gameboard();
-  const player = new AiPlayer(opponentBoard);
-  player.attack();
-  expect(opponentBoard.missedAttacks.length).toEqual(1);
+  const humanBoard = new Gameboard();
+  const ai = new AiPlayer(humanBoard);
+  ai.attack();
+  expect(humanBoard.missedAttacks.length).toEqual(1);
 });
 
 test('Ai player does not repeat attacks on same square', () => {
-  const opponentBoard = new Gameboard();
-  const player = new AiPlayer(opponentBoard);
+  const humanBoard = new Gameboard();
+  const ai = new AiPlayer(humanBoard);
   for (let i = 0; i < 105; i += 1) {
-    player.attack();
+    ai.attack();
   }
-  expect(opponentBoard.missedAttacks.length).toEqual(100);
+  expect(humanBoard.missedAttacks.length).toEqual(100);
+});
+
+test('Ai player plays automatically after human player', () => {
+  const aiBoard = new Gameboard();
+  const humanBoard = new Gameboard();
+  const ai = new AiPlayer(humanBoard);
+  const human = new HumanPlayer(ai, aiBoard);
+  human.attack([0, 0]);
+  expect(aiBoard.missedAttacks.length).toBe(1);
+  expect(humanBoard.missedAttacks.length).toBe(1);
 });
