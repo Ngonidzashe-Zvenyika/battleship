9f986a7aea7a2a6d4974c12848bb4f78bd8756f2
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-11-06 04:59:16 +0200

    Write tests for players to report game status after each move

--- tests/players.test.js
+++ tests/players.test.js
@@ -20,7 +20,8 @@ test("Human player can't attack same square twice", () => {
   const ai = new AiPlayer(humanBoard);
   const human = new HumanPlayer(ai, aiBoard);
   human.attack([0, 0]);
-  expect(human.attack([0, 0])).toBe(false);
+  const gameStatus = human.attack([0, 0]);
+  expect(gameStatus.moveValid).toBe(false);
 });
 
 test('Ai player attacks random square on enemy board', () => {
@@ -47,4 +48,47 @@ test('Ai player plays automatically after human player', () => {
   human.attack([0, 0]);
   expect(aiBoard.missedAttacks.length).toBe(1);
   expect(humanBoard.missedAttacks.length).toBe(1);
+});
+
+test('Human player reports the status of the game after playing a move', () => {
+  const aiBoard = new Gameboard();
+  aiBoard.place([0, 0], 'carrier', 'horizontal');
+  const humanBoard = new Gameboard();
+  const ai = new AiPlayer(humanBoard);
+  const human = new HumanPlayer(ai, aiBoard);
+  for (let i = 0; i < 4; i += 1) {
+    const gameStatus = human.attack([0, i]);
+    expect(gameStatus).toEqual({
+      gameWinner: null,
+      gameOver: false,
+      moveValid: true,
+    });
+  }
+  const gameStatus = human.attack([0, 4]);
+  expect(gameStatus).toEqual({
+    gameWinner: 'human',
+    gameOver: true,
+    moveValid: true,
+  });
+});
+
+test('Ai player reports the status of the game after playing a move', () => {
+  const humanBoard = new Gameboard();
+  humanBoard.place([0, 0], 'carrier', 'horizontal');
+  const ai = new AiPlayer(humanBoard);
+  let result;
+  let gameOver = false;
+  let count = 0;
+  while (!gameOver) {
+    const gameStatus = ai.attack();
+    result = gameStatus;
+    gameOver = gameStatus.gameOver;
+    count += 1;
+  }
+  expect(count).toBeLessThanOrEqual(100);
+  expect(result).toEqual({
+    gameWinner: 'ai',
+    gameOver: true,
+    moveValid: true,
+  });
 });
