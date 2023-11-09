bf9cf747bdbb6d73fbac372bd2b79367e673d5ed
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-11-09 11:10:03 +0200

    Write tests to ensure that ships can't be placed on occupied squares

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -20,6 +20,15 @@ test('Fail to place carrier horizontally at [0,6] ', () => {
   expect(location.ship).toBeNull();
 });
 
+test('Fail to place ship on already occupied [0,0] - [0,4]', () => {
+  const gameboard = new Gameboard();
+  gameboard.place([0, 0], 'carrier', 'horizontal');
+  for (let i = 0; i < 5; i += 1) {
+    gameboard.place([0, i], 'carrier', 'horizontal');
+    expect(gameboard.ships.length).toBe(1);
+  }
+});
+
 test('Place carrier vertically at [0,0] ', () => {
   const gameboard = new Gameboard();
   gameboard.place([0, 0], 'carrier', 'vertical');
@@ -38,6 +47,37 @@ test('Fail to place carrier vertically at [6,0] ', () => {
   gameboard.place([6, 0], 'carrier', 'vertical');
   const location = gameboard.board[6][0];
   expect(location.ship).toBeNull();
+});
+
+test('Fail to place ship on already occupied [0,0] - [4,0]', () => {
+  const gameboard = new Gameboard();
+  gameboard.place([0, 0], 'carrier', 'vertical');
+  for (let i = 0; i < 5; i += 1) {
+    gameboard.place([i, 0], 'carrier', 'vertical');
+    expect(gameboard.ships.length).toBe(1);
+  }
+});
+
+test('Returns the target cells if evaluation of placement is valid', () => {
+  const gameboard = new Gameboard();
+  const evaluation = gameboard.evaluatePlacement([0, 0], 'vertical', 5);
+  expect(evaluation.valid).toBe(true);
+  gameboard.place([0, 0], 'carrier', 'vertical');
+  for (let i = 0; i < 5; i += 1) {
+    const targetCell = evaluation.targetCells.find((cell) => {
+      const [x, y] = cell;
+      return x === i && y === 0;
+    });
+    expect(targetCell).toBeTruthy();
+  }
+});
+
+test("Returns 'targetCells: null' if evaluation of placement is invalid", () => {
+  const gameboard = new Gameboard();
+  gameboard.place([0, 0], 'carrier', 'vertical');
+  const evaluation = gameboard.evaluatePlacement([0, 0], 'vertical', 5);
+  expect(evaluation.valid).toBe(false);
+  expect(evaluation.targetCells).toBeNull();
 });
 
 test('Receive attack on empty square [0,0]', () => {
